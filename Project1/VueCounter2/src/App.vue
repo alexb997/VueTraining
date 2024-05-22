cb55075a49c227e1a3d05f8ba7bcd7f0726d1b3f
alexb997 <alex_alexandru_997@yahoo.ro>
2024-05-22 20:56:04 +0300

    basics

---
+++ Project1/VueCounter2/src/App.vue
@@ -0,0 +1,58 @@
+<!-- Composition API
+  <script setup>
+import { ref } from "vue";
+
+let count = ref(0);
+
+const addToCount = () => {
+  count.value += 1;
+};
+
+const substractFromCount = () => {
+  count.value -= 1;
+};
+</script>
+-->
+<!-- Options API -->
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    addToCount() {
+      this.count += 1;
+    },
+    substractFromCount() {
+      this.count -= 1;
+    },
+  },
+};
+</script>
+
+<template>
+  <main>
+    <container class="container">
+      <h4>The current count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="substractFromCount">-</button>
+      <button @click="addToCount">+</button>
+    </container>
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.container {
+  text-align: center;
+}
+</style>
